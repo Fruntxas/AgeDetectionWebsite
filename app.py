d786031ec83c67ce384c1b07d838fff25c15f935
felix faehnrich <felix@faehnrich.de>
2021-06-11 21:26:59 +0200

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -8,35 +8,50 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras import models
 import tempfile
+import cv2
 
 #BASE_URL = 'https://agedetection-tyxhjmug3a-ew.a.run.app/image'  #Tiago
 BASE_URL = 'https://agedetection-m2ianlcoya-ew.a.run.app/image'   #Felix
 
-#Functions
 
 
 #Page Layout
+
 st.set_page_config(layout="wide")
 '''# Age Detection'''
+col1, col2,col3,col4= st.beta_columns((3, 1,1,1))
 
-st.title("Upload + Classification Example")
+with col1:
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', width=300)
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+        files = {'file':uploaded_file.getvalue()
+                }
 
-    files = {'file':uploaded_file.getvalue()
-            }
+        response = requests.post(
+            BASE_URL,
+            files=files
+        )
 
-    response = requests.post(
-        BASE_URL,
-        files=files
-    )
+with col3:
+    if st.button('Predict the Age!'):
+        st.write("Thinking...")
+        st.write(response.json()['Guess'])
+        st.balloons()
 
-    # st.write("")
-    st.write("Classifying...")
-    st.write(response.json()['Guess'])
-    st.balloons()
+
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
+
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
 
 
