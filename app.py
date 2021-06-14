c339ce71ce08010052fd93ff9c05ea789da2c05c
felix faehnrich <felix@faehnrich.de>
2021-06-14 22:56:34 +0200

    Changes

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import tempfile
-#import cv2
-#from autocrop import Cropper
+import cv2
+from autocrop import Cropper
 
 #BASE_URL = 'https://agedetection-tyxhjmug3a-ew.a.run.app/image'  #Tiago
 BASE_URL = 'https://agedetection-m2ianlcoya-ew.a.run.app/image'   #Felix
@@ -17,47 +17,60 @@ BASE_URL = 'https://agedetection-m2ianlcoya-ew.a.run.app/image'   #Felix
 #Page Layout
 
 st.set_page_config(layout="wide")
-'''# Age Detection!'''
-#col1, col2, col3, col4= st.beta_columns((4, 1,1,1))
+
+st.markdown("<h1 style='text-align: center;'>Age Detection</h1>", unsafe_allow_html=True)
+
+col1, col2 = st.beta_columns((1,1))
+files=None
+
+with col1:
+
+    st.markdown("<h1 style='text-align: center;'>1) Upload an Image!</h1>", unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("", type="jpg")
 
 
+    if uploaded_file is not None:
+        with open("tmp.png", "wb+") as data:
+            data.write(uploaded_file.read())
 
-st.title("Upload an Image")
-uploaded_file = st.file_uploader("", type="jpg")
-
-
-if uploaded_file is not None:
-    with open("tmp.png", "wb+") as data:
-        data.write(uploaded_file.read())
-
-    st.write('Uploaded Image')
-    image_normal = Image.open(uploaded_file)
-    #st.image(image_normal, caption='', width=300)
-
-    #st.write('Cropped Image')
-    #cropper = Cropper(width=100, height=100)
-    # Get a Numpy array of the cropped image
-    #cropped_array = cropper.crop("tmp.png")
-    # Save the cropped image with PIL
-    #cropped_image = Image.fromarray(cropped_array)
-    #cropped_image.save('cropped.png')
-    #image_cropped = Image.open('cropped.png')
-    #st.image(image_cropped, caption='', use_column_width=False)
+        #st.markdown("<h2 style='text-align: center;'>This is your uploaded Image:</h1>", unsafe_allow_html=True)
+        '''## This is your uploaded Image:'''
+        image_normal = Image.open(uploaded_file)
+        st.image(image_normal, caption='', width=300)
+        cropper = Cropper(width=300, height=300)
+        cropped_array = cropper.crop("tmp.png")
+        cropped_image = Image.fromarray(cropped_array)
+        cropped_image.save('cropped.png')
+        image_cropped = Image.open('cropped.png')
 
 
-    files = {'file':uploaded_file.getvalue()
-            }
+        files = {'file':uploaded_file.getvalue()
+                }
+#if files != None:
+
+    with col2:
+        m = st.markdown("""
+        <style>
+        div.stButton > button:first-child {
+            background-color: rgb(240, 242, 246);
+            height: 10em;
+            width: 30em;
 
 
+        }
+        </style>""", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center;'>2) Push the Button!</h1>", unsafe_allow_html=True)
+        if st.button('Predict the Age'):
+            '''## First we crop and resize your image'''
+            st.image(image_cropped, caption='', use_column_width=False)
 
-if st.button('Predict the Age!'):
+            response = requests.post(
+                BASE_URL,
+                files=files
+            )
 
-    response = requests.post(
-        BASE_URL,
-        files=files
-    )
-    st.write("Your age is between: ")
-    st.write(response.json()['Initial Age Bin'])
-    st.write('And we think you look like:')
-    st.write(response.json()['Weighted Guess'])
-    st.balloons()
+            st.write("Your age is between: ")
+            st.write(response.json()['Initial Age Bin'])
+            st.write('And we think you look like:')
+            st.write(int(response.json()['Weighted Guess']))
+            st.balloons()
